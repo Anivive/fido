456cc4d06be8420e2ead8685ee35a977448aa489
Colin Duffy <colin@tomorrowevening.com>
2021-02-15 13:28:40 -0800

    determine camera FOV

--- src/fido/ExportCamera.js
+++ src/fido/ExportCamera.js
@@ -4,9 +4,16 @@ function ExportCamera(props, exportOptions) {
     var codeNames = ["blur", "DOF", "focus", "zoom"];
     var data      = {};
     var i, total  = aeNames.length;
-    for(i = 0; i < total; ++i) {
-        data[ codeNames[i] ] = options.property( aeNames[i] ).value;
+    for (i = 0; i < total; ++i) {
+        data[codeNames[i]] = options.property(aeNames[i]).value;
     }
-    data.timeline = exportProps( props, aeNames, codeNames, exportOptions );
+
+    // Determine FOV
+    var comp = props.containingComp;
+    var width = comp.width;
+    var aspect = width / comp.height;
+    data.fov = Number((2 * Math.atan(width / aspect / (2 * data.zoom)) * (180 / Math.PI)).toFixed(3));
+
+    data.timeline = exportProps(props, aeNames, codeNames, exportOptions);
     return data;
 };

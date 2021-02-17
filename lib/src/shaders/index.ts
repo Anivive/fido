171236b027eaa69fcd245018a8325d6ebe8ef3e4
Colin Duffy <colin@tomorrowevening.com>
2021-02-16 18:35:00 -0800

    materials should import their shaders now

---
+++ lib/src/shaders/index.ts
@@ -0,0 +1,14 @@
+declare module '*.glsl' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.vert' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.frag' {
+  const content: any;
+  export default content;
+}
